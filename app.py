9a2fc6d681243f76b97593834fde5d5fc9793929
ellaj19 <ellaj19@nycstudents.net>
2025-10-28 10:17:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,40 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-for i in range(len(data)):
-    print(data[i]["title"])
+""" for i in range(len(data)):
+    print(data[i]["title"])  """
 
-list = [1,2,3,4,5]
-for i in range(list):
-    print(list[i])
+
+""" answer = input("after what year do you want to see the movies of?")
+while answer >= 1961:
+    check = data >= answer
+    print(check[answer]["title"]) """
+
+genrelist = ["Drama",
+             "Crime",
+             "War",
+             "Comedy",
+             "Fantasy",
+             "Independent",
+             "Noir",
+             "Thriller",
+             "Historical",
+             "Action",
+             "Romance",
+             "Western",
+             "Mystery",
+             "Musical",
+             "Science Fiction",
+             "Disaster",
+             "Political",
+             "Family",
+             "Adventure",
+             "Biography",
+             "Suspense",
+             "Animated"]
+print(genrelist)
+answer = input("what genre do you want to search?")
+for i in range(0, len(data)):
+    if data[i]["genres"] == answer:
+        print(data[i]["title"])
+        
